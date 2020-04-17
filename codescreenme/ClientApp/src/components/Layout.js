ba03178d65f2cafa266b045eb88e9773d2391935
artemfursenko <artem.fursenko@sitecore.com>
2020-04-17 21:19:33 +0200

    Revert "Added cookie consent (#22)"
    This reverts commit 0b1033e11582565edb44d1dee0a93fb586760ee1.

--- codescreenme/ClientApp/src/components/Layout.js
+++ codescreenme/ClientApp/src/components/Layout.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import CookieConsent from "react-cookie-consent";
 import { NavMenu } from './NavMenu';
 import { Footer } from './Footer';
-import { Link } from 'react-router-dom';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -13,10 +11,6 @@ export class Layout extends Component {
       <>
         <NavMenu />
         {this.props.children}
-        <CookieConsent
-          buttonText="Acknowledge">
-          This website uses cookies according to the <Link to="/articles/cookie">cookie policy</Link>.By proceeding further, you are accepting our <Link to="/articles/cookie">cookie policy</Link>.
-        </CookieConsent>
         <Footer />
       </>
     );
